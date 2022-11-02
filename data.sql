6552fb2b15dfd0f151c787106e5d2546aa467279
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-02 22:26:51 +0300

    Add 7 rows to the animal table

--- data.sql
+++ data.sql
@@ -7,3 +7,5 @@ Animal: Her name is Devimon. She was born on May 12th, 2017, and currently weigh
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '03-2-2020', 0, true, 10.23);
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '15-11-2018', 2, true, 8), ('Pikachu', '07-1-2021', 1, false, 15.04), ('Devimon', '12-5-2017', 5, true, 11);
+ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '08-2-2020', 0, false, -11), ('Plantmon', '15-11-2021', 2, true, -5.7), ('Squirtle', '02-4-1993', 3, false, -12.13), ('Angemon', '12-1-2005', 1, true, -45), ('Boarmon', '07-6-2005', 7, true, 20.4), ('Blossom', '13-10-1998', 3, true, 17), ('Ditto', '14-05-2022', 4, true, 20);
+ 
