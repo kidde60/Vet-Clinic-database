17249db4c3a89a2a946fbd54a0ddd07e8bdbe421
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-02 11:12:59 +0300

    make changes

--- queries.sql
+++ queries.sql
@@ -13,7 +13,7 @@ Find all animals with a weight between 10.4kg and 17.3kg (including the animals 
 SELECT name from animals WHERE name like '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
-SELECT name from animals WHERE date_of_birth >= '2016-01-10' and date_of_birth < '2019-12-31';
+SELECT name from animals WHERE date_of_birth >= '2016-01-01' and date_of_birth <= '2019-12-31';
  
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name from animals WHERE neutered = true and escape_attempts < 3;
